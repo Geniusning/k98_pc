2fb0c94f4d3503e3741582d81d9136763a9f234c
Geniusning <690984649@qq.com>
2018-11-10 17:02:02 +0800

    新建项目

---
+++ src/store/getters.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: liuning 
+ * @Date: 2018-11-10 15:53:28 
+ * @Last Modified by:   liuning 
+ * @Last Modified time: 2018-11-10 15:53:28 
+ */
---
+++ src/store/mutation-types.js
@@ -0,0 +1,2 @@
+//获得公众号地址
+export const GET_URL = "GET_URL"
